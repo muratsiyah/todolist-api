0bea563582b3a92aa6ae049d02f475c48e06370a
MuratKara <muratkara.mk0@gmail.com>
2020-03-12 10:49:26 +0300

    initial commit

---
+++ src/test/java/com/murat/siyah/todolistapi/TodolistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murat.siyah.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
