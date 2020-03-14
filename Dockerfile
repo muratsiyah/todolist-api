206b64a60a0f1d87748bcc33da9d36cd79786420
MuratKara <muratkara.mk0@gmail.com>
2020-03-14 21:11:45 +0300

    Dockerfile and init.sh edited

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
-FROM maven:3-alpine AS build-project
-
-ADD . ./todolist-api
-WORKDIR /todolist-api
-RUN mvn clean install -DskipTests=true
-
 FROM openjdk:8-jre-alpine
+RUN apk add curl
 EXPOSE 8080
 WORKDIR /app
 COPY --from=build-project ./todolist-api/target/todolist-api-*.jar ./todolist-api.jar
-CMD ["init.sh"]
+COPY ./init.sh /
+ENTRYPOINT [ "/bin/sh", "/init.sh" ]
--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' couchbase:8091)" != "200" ]]; do sleep 5; done'
+/bin/sh -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' couchbase:8091)" != "200" ]]; do echo "WAITING: COUCHBASE SERVER IS NOT READY, IT WILL TAKE 2-3 MINS!" && sleep 5; done'
+echo "OK: COUCHBASE LOOKS READY, APPLICATION STARTING!"
+sleep 10
 java -jar todolist-api.jar
+echo "OK: APPLICATION STARTED!"
