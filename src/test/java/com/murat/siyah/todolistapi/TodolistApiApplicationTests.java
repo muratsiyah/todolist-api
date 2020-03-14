2e483fccb8689d3752474f3300eb6ba61e300cf3
MuratKara <muratkara.mk0@gmail.com>
2020-03-14 16:38:27 +0300

    docker compose added

--- src/test/java/com/murat/siyah/todolistapi/TodolistApiApplicationTests.java
+++ src/test/java/com/murat/siyah/todolistapi/TodolistApiApplicationTests.java
@@ -2,9 +2,7 @@ package com.murat.siyah.todolistapi;
 
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class TodolistApiApplicationTests {
 
     @Ignore
